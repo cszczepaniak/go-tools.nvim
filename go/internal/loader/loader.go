c91c611c292c3f10d001d84fe29fabf8ae19cf1a
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-06-02 20:54:53 -0500

    add some stats

--- go/internal/loader/loader.go
+++ go/internal/loader/loader.go
@@ -6,9 +6,11 @@ import (
 	"go/parser"
 	"go/token"
 	"sync"
+	"sync/atomic"
 
 	"github.com/cszczepaniak/go-tools/internal/asthelper"
 	"github.com/cszczepaniak/go-tools/internal/file"
+	"github.com/cszczepaniak/go-tools/internal/logging"
 	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/go/packages"
 )
@@ -23,6 +25,11 @@ type Loader struct {
 
 	fileOnce func() (*ast.File, error)
 	pkgOnce  func() (*packages.Package, error)
+
+	nFilesParsed        atomic.Int64
+	whenWasMyFileParsed atomic.Int64
+	nFunctionsStripped  atomic.Int64
+	totalFunctionsSeen  atomic.Int64
 }
 
 func New(
@@ -80,9 +87,11 @@ func (l *Loader) parseFileForLoadPkg(
 	filepath string,
 	src []byte,
 ) (*ast.File, error) {
+	l.nFilesParsed.Add(1)
 	var f *ast.File
 	var err error
 	if filepath == l.contents.AbsPath {
+		l.whenWasMyFileParsed.Store(l.nFilesParsed.Load())
 		f, err = l.ParseFile()
 	} else {
 		f, err = parser.ParseFile(
@@ -97,13 +106,15 @@ func (l *Loader) parseFileForLoadPkg(
 	}
 
 	for _, decl := range f.Decls {
-		if asthelper.NodeContains(decl, l.Pos) {
-			continue
-		}
-
 		// Strip away the bodies of all function declarations that don't contain our position. This
 		// speeds up type checking.
 		if fnDecl, ok := decl.(*ast.FuncDecl); ok {
+			l.totalFunctionsSeen.Add(1)
+			if asthelper.NodeContains(decl, l.Pos) {
+				continue
+			}
+
+			l.nFunctionsStripped.Add(1)
 			fnDecl.Body = nil
 		}
 	}
@@ -130,6 +141,13 @@ func (l *Loader) loadPackage() (*packages.Package, error) {
 	if len(pkgs) != 1 {
 		panic(`should be unreachable; we only specify one package in the given pattern`)
 	}
+
+	logging.WithFields(map[string]any{
+		"nFiles":    l.nFilesParsed.Load(),
+		"myFileIdx": l.whenWasMyFileParsed.Load(),
+		"nFuncs":    l.totalFunctionsSeen.Load(),
+		"nStripped": l.nFunctionsStripped.Load(),
+	}).Debug("package load stats")
 
 	return pkgs[0], nil
 }
